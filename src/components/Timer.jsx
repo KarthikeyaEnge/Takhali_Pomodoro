4a83b0c69fb0213dc8d7a29e5fc48c81a7139e66
Enge Karthikeya <karthikeya.enge@hotmail.com>
2023-09-18 12:41:39 +0530

    Added labels fot the buttons for accessability

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -45,28 +45,47 @@ const Timer = ({ timelimit, timetype }) => {
             }}
           >
             <button
-              className="absolute flex-shrink-0 bg-slate-950 backdrop-blur-lg bg-opacity-70 shadow-inner shadow-cyan-900 text-4xl text-cyan-400 rounded-full p-3 active:shadow-none top-10 -left-2"
+              className="absolute group flex-shrink-0 bg-slate-950 backdrop-blur-lg bg-opacity-70 shadow-inner shadow-cyan-900 text-4xl text-cyan-400 rounded-full p-3 active:shadow-none top-10 -left-2"
               onClick={stoptimer}
             >
+              <label
+                htmlFor="play"
+                className="absolute hidden group-hover:block text-sm dark:bg-slate-800 dark:text-slate-200 font-poppins-500 text-slate-800 bg-slate-400 rounded-md -top-9 px-4 py-2"
+              >
+                Pause
+              </label>
               <FaPause />
             </button>
             <button
-              className="absolute flex-shrink-0 bg-slate-950 backdrop-blur-lg bg-opacity-70 shadow-inner shadow-cyan-900 text-4xl text-cyan-400 rounded-full p-3 active:shadow-none top-10 -right-2"
+              className="absolute group flex-shrink-0 bg-slate-950 backdrop-blur-lg bg-opacity-70 shadow-inner shadow-cyan-900 text-4xl text-cyan-400 rounded-full p-3 active:shadow-none top-10 -right-2"
               onClick={resettimer}
             >
+              <label
+                htmlFor="play"
+                className="absolute hidden group-hover:block text-sm dark:bg-slate-800 dark:text-slate-200 font-poppins-500 text-slate-800 bg-slate-400 rounded-md -top-9 px-4 py-2"
+              >
+                Reset
+              </label>
               <FaArrowsRotate />
             </button>
             <h1 className="flex-shrink-0 self-center text-6xl">{showtime}</h1>
 
             <button
-              className={`absolute flex-shrink-0 bg-slate-950 backdrop-blur-lg bg-opacity-70 shadow-inner shadow-cyan-900 text-4xl text-cyan-400 rounded-full p-3 active:shadow-none mt-10 -bottom-5 ${
+              className={`absolute flex-shrink-0 bg-slate-950 backdrop-blur-lg bg-opacity-70 shadow-inner shadow-cyan-900 text-4xl text-cyan-400 rounded-full p-3 active:shadow-none mt-10 -bottom-5 group ${
                 timerstart
                   ? " btn-disabled cursor-not-allowed"
                   : "btn-active cursor-pointer"
               }`}
               onClick={starttimer}
               disabled={timerstart}
+              name="play"
             >
+              <label
+                htmlFor="play"
+                className="absolute hidden group-hover:block text-sm dark:bg-slate-800 dark:text-slate-200 font-poppins-500 text-slate-800 bg-slate-400 rounded-md -top-9 px-4 py-2"
+              >
+                Play
+              </label>
               <FaPlay />
             </button>
           </div>
