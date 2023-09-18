a92bda8581e0690c33c0211543b909fb5ea5213c
Enge Karthikeya <karthikeya.enge@hotmail.com>
2023-09-19 00:51:34 +0530

    Added the Manifest.json to convert into a pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,39 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      injectRegister: "auto",
+      manifest: {
+        theme_color: "#2fd3f4",
+        background_color: "#020a2c",
+        icons: [
+          {
+            purpose: "maskable",
+            sizes: "512x512",
+            src: "/icon.png",
+            type: "image/png",
+          },
+          {
+            purpose: "any",
+            sizes: "512x512",
+            src: "/icon.png",
+            type: "image/png",
+          },
+        ],
+        orientation: "portrait",
+        display: "standalone",
+        dir: "auto",
+        lang: "en-GB",
+        name: "Takhali Timer",
+        short_name: "Takhali",
+        start_url: "/",
+        scope: "/",
+        description: "A small Pomodoro Timer App for more productivity",
+      },
+    }),
+  ],
+});
